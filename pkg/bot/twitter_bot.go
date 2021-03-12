1c29a48db7c8ebf1038b99d599f55aca498b549f
Luca Di Carlo <ldc.luca.dicarlo@gmail.com>
2021-03-12 20:04:35 +0100

    feat: save

--- pkg/bot/twitter_bot.go
+++ pkg/bot/twitter_bot.go
@@ -13,7 +13,10 @@ import (
 var consumerKey string
 var consumerSecret string
 var callbackURL string
+var twitterAccessTokenSecret string
 var oauthToken string
+var requestSecret string
+var requestToken string
 var config oauth1.Config
 
 func init() {
@@ -24,8 +27,12 @@ func init() {
 	consumerKey = envs["twitter_consumer_key"]
 	consumerSecret = envs["twitter_comsumer_secret"]
 	callbackURL = envs["twitter_callback_url"]
+	twitterAccessTokenSecret = envs["twitter_access_token_secret"]
 
-	if consumerKey == "" || consumerSecret == "" || callbackURL == "" {
+	if consumerKey == "" ||
+		consumerSecret == "" ||
+		callbackURL == "" ||
+		twitterAccessTokenSecret == "" {
 		panic("Missing one of the env params")
 	}
 
@@ -40,8 +47,10 @@ func GetAuthURL() string {
 		Endpoint:       oauthTwitter.AuthorizeEndpoint,
 	}
 
-	requestToken, _, err := config.RequestToken()
+	rt, rs, err := config.RequestToken()
 	utils.ErrCheck(err)
+	requestToken = rt
+	requestSecret = rs
 	authorizationURL, err := config.AuthorizationURL(requestToken)
 	utils.ErrCheck(err)
 	return authorizationURL.String()
@@ -49,7 +58,7 @@ func GetAuthURL() string {
 
 func RegisterToken(newOauthToken string, tokenVerifier string) {
 	oauthToken = newOauthToken
-	token := oauth1.NewToken(oauthToken, tokenVerifier)
+	token := oauth1.NewToken(oauthToken, requestSecret)
 	httpClient := config.Client(oauth1.NoContext, token)
 	client := twitter.NewClient(httpClient)
 	tweets, resp, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
